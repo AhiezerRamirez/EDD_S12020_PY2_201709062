3f95902a8ab98f8fd2681652872e3f980cffb5f3
Ahiezer Ramirez <1773794720101@ingenieria.usac.edu.gt>
2020-04-28 02:14:11 -0600

    "fixed AVL"

--- src/Estructuras/ArbolAVL.java
+++ src/Estructuras/ArbolAVL.java
@@ -22,7 +22,7 @@ public class ArbolAVL {
         NodoAVL x = y.getIzq();
         NodoAVL t2=x.getDer();
         
-        x.setDer(x);
+        x.setDer(y);
         y.setIzq(t2);
         
         y.setAltura(max(altura(y.getIzq()),altura(y.getDer()))+1);
@@ -44,47 +44,62 @@ public class ArbolAVL {
     int obtenerBalance(NodoAVL N){
         if(N==null)
             return 0;
-        return altura(N.getIzq()) - altura(N.getDer());
+        return altura(N.getDer()) - altura(N.getIzq());
     }
     public NodoAVL insertar(NodoAVL nodo,String categoria){
         if(nodo == null)
             return (new NodoAVL(categoria));
-        else if(categoria.compareTo(nodo.getCategoria()) < 0)
+        else if(categoria.compareToIgnoreCase(nodo.getCategoria()) < 0)
             nodo.setIzq(insertar(nodo.getIzq(),categoria));
-        else if(categoria.compareTo(nodo.getCategoria()) > 0)
+        else if(categoria.compareToIgnoreCase(nodo.getCategoria()) > 0)
             nodo.setDer(insertar(nodo.getDer(),categoria));
         else
             return nodo;
         nodo.setAltura(1+ max(altura(nodo.getIzq()),altura(nodo.getDer())));
         int balance=obtenerBalance(nodo);
         
-        if(balance > 1 && (categoria.compareTo(nodo.getIzq().getCategoria()) < 0))
-            return rotarDer(nodo);
-        if(balance < -1 && (categoria.compareTo(nodo.getDer().getCategoria()) > 0))
+        if(balance > 1 && (categoria.compareToIgnoreCase(nodo.getDer().getCategoria()) > 0))
             return rotarIzq(nodo);
-        if(balance > 1 && (categoria.compareTo(nodo.getIzq().getCategoria()) > 0)){
-            nodo.setIzq(rotarIzq(nodo.getIzq()));
+        if(balance < -1 && (categoria.compareToIgnoreCase(nodo.getIzq().getCategoria()) < 0))
             return rotarDer(nodo);
-        }
-        if(balance < -1 && (categoria.compareTo(nodo.getDer().getCategoria()) < 0)){
+        if(balance > 1 && (categoria.compareToIgnoreCase(nodo.getDer().getCategoria()) < 0)){
             nodo.setDer(rotarDer(nodo.getDer()));
             return rotarIzq(nodo);
+        }
+        if(balance < -1 && (categoria.compareToIgnoreCase(nodo.getIzq().getCategoria()) > 0)){
+            nodo.setIzq(rotarIzq(nodo.getIzq()));
+            return rotarDer(nodo);
         }
         
             
         return nodo;
     }
+    static int nullcount=0;
+    private void imprimirNull(String clave, int nul,StringBuilder d){
+        d.append("null").append(nul).append(" [shape=point];\n\t");
+        d.append("\"").append(clave).append("\" -> null").append(nul).append("\n\t");
+    }
     private void graficarAux(NodoAVL nodo, StringBuilder s){
-        if(nodo.getDer()!=null){
-            s.append(nodo.getCategoria()).append(" -> ").append(nodo.getDer().getCategoria()).append("\n\t");
-            graficarAux(nodo.getDer(), s);
-        }
+        
         if(nodo.getIzq()!=null){
-            s.append(nodo.getCategoria()).append(" -> ").append(nodo.getIzq().getCategoria()).append("\n\t");
+            s.append("\"").append(nodo.getCategoria()).append("\" -> \"").append(nodo.getIzq().getCategoria()).append("\"\n\t");
+            //System.out.println(nodo.getAltura()+" "+ nodo.getIzq().getAltura());
+            //System.out.println(nodo.getCategoria()+" "+ nodo.getIzq().getCategoria());
             graficarAux(nodo.getIzq(), s);
+        }else{
+            imprimirNull(nodo.getCategoria(), nullcount++, s);
+        }
+        if(nodo.getDer()!=null){
+            s.append("\"").append(nodo.getCategoria()).append("\" -> \"").append(nodo.getDer().getCategoria()).append("\"\n\t");
+            //System.out.println(nodo.getAltura()+" "+ nodo.getDer().getAltura());
+            //System.out.println(nodo.getCategoria()+" "+ nodo.getDer().getCategoria());
+            graficarAux(nodo.getDer(), s);
+        }else{
+            imprimirNull(nodo.getCategoria(), nullcount++, s);
         }
     }
     public String obtenerCodigoDot(NodoAVL raiz){
+        nullcount=0;
         StringBuilder cadena=new StringBuilder();
         cadena.append("digraph BST {\n\tnode [fontname=\"Arial\"];\n \t");
         if(raiz==null){
@@ -96,5 +111,56 @@ public class ArbolAVL {
         }
         cadena.append("}");
         return cadena.toString();
+    }
+    private NodoAVL getMinVal(NodoAVL node){
+        NodoAVL cur=node;
+        while (cur.getIzq()!=null) {            
+            cur=cur.getIzq();
+        }
+        return cur;
+    }
+    public NodoAVL borrar(NodoAVL raiz,String categoria){
+        if(raiz==null)
+            return raiz;
+        if(categoria.compareToIgnoreCase(raiz.getCategoria()) < 0)
+            raiz.setIzq(borrar(raiz.getIzq(),categoria));
+        else if(categoria.compareToIgnoreCase(raiz.getCategoria()) > 0)
+            raiz.setDer(borrar(raiz.getDer(),categoria));
+        else{
+            if(raiz.getIzq()==null || raiz.getDer()==null){
+                NodoAVL temp=null;
+                if(temp==raiz.getIzq())
+                    temp=raiz.getDer();
+                else
+                    temp=raiz.getIzq();
+                if(temp==null){
+                    temp=raiz;
+                    raiz=null;
+                }else
+                    raiz=temp;
+            }else{
+                NodoAVL temp=getMinVal(raiz.getDer());
+                raiz.setCategoria(temp.getCategoria());
+                raiz.setDer(borrar(raiz.getDer(),categoria));
+            }
+        }
+        if(raiz==null)
+            return raiz;
+        raiz.setAltura(max(altura(raiz.getIzq()), altura(raiz.getDer()))+1);
+        int balance =obtenerBalance(raiz);
+        if(balance > 1 && (obtenerBalance(raiz.getDer()) > 0))
+            return rotarIzq(raiz);
+        if(balance > 1 && obtenerBalance(raiz.getDer()) <= 0){
+            raiz.setDer(rotarDer(raiz.getDer()));
+            return rotarIzq(raiz);
+        }
+        if(balance < -1 && obtenerBalance(raiz.getIzq()) < 0)
+            return rotarDer(raiz);
+        if(balance < -1 && obtenerBalance(raiz.getIzq()) >=0){
+            raiz.setIzq(rotarIzq(raiz.getIzq()));
+            return rotarDer(raiz);
+        }
+        
+        return raiz;
     }
 }
